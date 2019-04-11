1fcae1717f42c52895c9deedc5b6148a4dccd960
dean.yu <yutu@qmap.com.cn>
2019-04-11 10:44:04 +0800

    - add pc control.

---
+++ Lingang.City.Brain/Controllers/PCControlController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chimelong.Resort.Web.Controllers
+{
+    public class PCControlController : Controller
+    {
+        // GET: PCControl
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
