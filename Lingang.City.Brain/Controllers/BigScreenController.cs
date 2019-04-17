067b52a5df3e0c2eba1a474df9066ba206a299b1
dean.yu <yutu@qmap.com.cn>
2019-04-17 10:13:19 +0800

    - refine url formatting.

--- Lingang.City.Brain/Controllers/BigScreenController.cs
+++ Lingang.City.Brain/Controllers/BigScreenController.cs
@@ -1,13 +1,14 @@
-﻿using System.Web.Mvc;
-using NLog;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
 namespace Lingang.City.Brain.Web.Controllers
 {
-
     public class BigScreenController : Controller
     {
-        Logger logger = LogManager.GetCurrentClassLogger();
-        
+        // GET: BigScreen
         public ActionResult Index()
         {
             return View();
--- Lingang.City.Brain/Scripts/Shared/Config.js
+++ Lingang.City.Brain/Scripts/Shared/Config.js
@@ -1,9 +1,9 @@
 ﻿define(["util"], function (util) {
 //define( function () {
     return {
-        WebServiceUrl: "http://" + location.host + "/LingangCityBrainV3/",
-        WebServiceUrlForControl: "http://192.168.3.199/LingangCityBrainV3/",
-        HtmlUrl: "/LingangCityBrainV3/View/",
+        WebServiceUrl: "http://" + location.host + "/LingangCityBrain/",
+        WebServiceUrlForControl: "http://192.168.3.199/LingangCityBrain/",
+        HtmlUrl: "/LingangCityBrain/View/",
         InterfaceUrl: "http://47.101.181.131:8091/",  //接口地址
         InterfaceUrl_garden: "http://47.101.181.131:8095/",  //接口地址
         InterfaceUrl_parking: "http://47.101.181.131:8093/",
