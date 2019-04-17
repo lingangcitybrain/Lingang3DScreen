7b3592e879a00ad6c43800b6f3ec1837c7e8f87b
LiuPing1993 <462684972@qq.com>
2019-04-17 16:00:43 +0800

    楼宇企业列表默认样式

--- Lingang.City.Brain/Scripts/Modules/Industry/Garden/gl_GardenBuilding.js
+++ Lingang.City.Brain/Scripts/Modules/Industry/Garden/gl_GardenBuilding.js
@@ -283,8 +283,8 @@
             gl_GardenBuildingAjax.getCompanyData(function (result) {
                 var html = "";
                 for (var i = 0; i < result.length; i++) {
-                    html+=' <li class="cy-ly-rr1-li active">'+
-                    '<div class="cy-ly-rr1-lidiv clearfix active">'+
+                    html+=' <li class="cy-ly-rr1-li ">'+
+                    '<div class="cy-ly-rr1-lidiv clearfix ">'+
                         '<span class="cy-ly-rr1-num">00'+(i+1)+'</span>'+
                         '<span class="cy-ly-rr1-name">'+result[i].companyName+'</span>'+
                         '<span class="cy-ly-rr1-date">' + result[i].preYearOutputValue + '万元</span>' +
