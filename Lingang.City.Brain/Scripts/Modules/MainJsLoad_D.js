ccb14e240aab427fcc859a28a7356e8f231064ed
dean.yu <yutu@qmap.com.cn>
2019-04-24 11:59:44 +0800

    - add map control.

--- Lingang.City.Brain/Scripts/Modules/MainJsLoad_D.js
+++ Lingang.City.Brain/Scripts/Modules/MainJsLoad_D.js
@@ -7,16 +7,16 @@ function (con, com, control_Ajax, s_Echart, s_Main, t_Main, e_Main, g_Main, b_Ma
 
         mainCameraUpdate: function () {
             Q3D.globalCamera().onCameraUpdate(1, function () {
-                var mainCamera = Q3D.globalCamera();
-                currPosV3d = mainCamera.getAbsPos();
-                currOriV3 = mainCamera.getOrientation()
+                    var mainCamera = Q3D.globalCamera();
+                    currPosV3d = mainCamera.getAbsPos();
+                    currOriV3 = mainCamera.getOrientation()
 
-                var jsondata = {
-                    "xyz": currPosV3d.x + ',' + currPosV3d.y + ',' + currPosV3d.z,
-                    "angle": currOriV3.x + ',' + currOriV3.y + ',' + currOriV3.z,
-                };
-                control_Ajax.sendMapControlInfo(jsondata); //发送控制命令
-            })
+                    var jsondata = {
+                        "xyz": currPosV3d.x + ',' + currPosV3d.y + ',' + currPosV3d.z,
+                        "angle": currOriV3.x + ',' + currOriV3.y + ',' + currOriV3.z,
+                    };
+                    control_Ajax.sendMapControlInfo(jsondata); //发送控制命令           
+            },50)
         },
     }
 })
