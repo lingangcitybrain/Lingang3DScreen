9b794b38ed9159be2487c15a2d00c8e28e5fcb9b
dean.yu <yutu@qmap.com.cn>
2019-05-08 16:17:31 +0800

    - rollback config.js

--- Lingang.City.Brain/Scripts/Shared/Config.js
+++ Lingang.City.Brain/Scripts/Shared/Config.js
@@ -12,9 +12,9 @@
         IsInterface: true,//是否走接口true:接口false本地
         AreaName: "gwh_xilou",
         CONFIG_NAME: "Example",
-        //SERVER_PATH: "http://" + location.host + "/mapdata3d/LingangCityBrain20190321",
-        SERVER_PATH: "http://192.168.3.199/mapdata3d/LingangCityBrain20190321",
-        LICENSE_SVR: "http://192.168.3.199/",
+        SERVER_PATH: "http://" + location.host + "/mapdata3d/LingangCityBrain20190321",
+        //SERVER_PATH: "http://192.168.3.207/mapdata3d/LingangCityBrain20190321",
+        //LICENSE_SVR: "http://www.q-map.com.cn:8280/",
         VideoInitialSize: null,//视频初始大小
         VideoInitialPosition: null,//视频初始位置
         isMonitorURL_service: false,
