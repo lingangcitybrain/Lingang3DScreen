8851f0c67c6840b2d2599802d05cfe3bd14f47ab
LiuPing1993 <462684972@qq.com>
2019-07-03 18:01:03 +0800

    人员分布

--- Lingang.City.Brain/Scripts/Modules/Society/s_Echart.js
+++ Lingang.City.Brain/Scripts/Modules/Society/s_Echart.js
@@ -479,12 +479,15 @@
                 }
                 //出临港 ,入临港相加
                 var outCarNum = 0, inCarNum = 0, carTotal = 0;
-                for (key1 in thisDayCarJson["出临港"]) {
-                    outCarNum += thisDayCarJson["出临港"][key1]
+                if (thisDayCarJson) {
+                    for (key1 in thisDayCarJson["出临港"]) {
+                        outCarNum += thisDayCarJson["出临港"][key1]
+                    }
+                    for (key2 in thisDayCarJson["入临港"]) {
+                        inCarNum += thisDayCarJson["入临港"][key2]
+                    }
                 }
-                for (key2 in thisDayCarJson["入临港"]) {
-                    inCarNum += thisDayCarJson["入临港"][key2]
-                }
+
                 carTotal = outCarNum + inCarNum;
 
                 $("#total_car").html(carTotal);
