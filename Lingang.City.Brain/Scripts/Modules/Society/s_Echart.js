fe3ee9333dd3cd35b65e8a0162dbdc71366d1b3b
dean.yu <yutu@qmap.com.cn>
2019-07-07 15:08:55 +0800

    - rollback senor interface.

--- Lingang.City.Brain/Scripts/Modules/Society/s_Echart.js
+++ Lingang.City.Brain/Scripts/Modules/Society/s_Echart.js
@@ -372,8 +372,8 @@
                 var data = require("s_Echart").cgqData;
                 //console.info(data);
                 //console.info(data.data.sensorNumList);
-                //data = data.data.sensorNumList;  //20190630改
-                data = data;
+                data = data.data.sensorNumList;  //20190630改
+                //data = data;
                 
                 $('#cgq-ywgy').html(data[6].sensorCount)
                 $('#cgq-zndt').html(data[5].sensorCount)
