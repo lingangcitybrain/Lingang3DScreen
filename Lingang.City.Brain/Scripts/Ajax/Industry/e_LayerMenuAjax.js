19c76ce586d7afadeb65ffed34f39b6710bb6036
qmap <qmap@DESKTOP-IJUF6C1>
2019-08-29 17:51:13 +0800

    园区产业poi，详情

---
+++ Lingang.City.Brain/Scripts/Ajax/Industry/e_LayerMenuAjax.js
@@ -0,0 +1,28 @@
+﻿define(["config", "e_LayerMenuData"], function (con, e_LayerMenuData) {
+    return {
+      
+        //产业区域信息
+        getIndustryData: function (callback) {
+            if (con.InterfaceUrl)//执行接口
+            {
+                $.ajax({
+                    type: "get",      //data 传送数据类型。post 传递 
+                    url: con.InterfaceUrl_DataStation + 'six/digit/industrDistribute',
+                    cache: false,
+                    dataType: 'json',  // 返回数据的数据类型json
+                    success: function (data) {
+                        require("el_EstateInfo").POIData = data;
+                        callback(data);
+                    },
+                    error: function () {
+                        //alert("数据传输错误");
+                    }
+                });
+            }
+            //else {//执行本地
+            //    require("g_Echart").tcfwData = g_EchartData.tcfwData.data;
+            //    callback();
+            //}
+        },
+    }
+    })
