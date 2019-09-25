325febca6e0d40cace45a0f7f8a9495a7dfeb0de
dean.yu <yutu@qmap.com.cn>
2019-09-25 11:33:45 +0800

    - change interface address for person and car realtime statistics.

--- Lingang.City.Brain/Scripts/Ajax/Society/s_EchartAjax.js
+++ Lingang.City.Brain/Scripts/Ajax/Society/s_EchartAjax.js
@@ -106,7 +106,8 @@
             if (con.IsInterface) {
                 $.ajax({
                     type: "POST",      //data 传送数据类型。post 传递 
-                    url: con.InterfaceUrl_DataStation + 'v1/comunity/people/personCarStatistics',
+                    // url: con.InterfaceUrl_DataStation + 'v1/comunity/people/personCarStatistics',
+                    url: con.InterfaceUrl_TourEvent + 'v1/haiyang/getPersonCarStatistics',
                     cache: false,
                     //data: post_data,  //传送的数据
                     dataType: 'json',  // 返回数据的数据类型json
