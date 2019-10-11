6435527ca312331d2900dc97f248e9602cf6edeb
dean.yu <yutu@qmap.com.cn>
2019-10-11 10:12:46 +0800

    - 临港现场修改出入园分时统计图表。日期由数字改为字符串。

--- Lingang.City.Brain/Scripts/Modules/Tour/t_EchartNew.js
+++ Lingang.City.Brain/Scripts/Modules/Tour/t_EchartNew.js
@@ -30,7 +30,32 @@
         }
         return dataArr.reverse();
     }
-    function MyDate(n) {
+ function MyDate(n) {
+        var n = n;
+        var d = new Date();
+        var year = d.getFullYear();
+        var mon = d.getMonth() + 1;
+        var day = d.getDate();
+        if (day <= n) {
+            if (mon > 1) {
+                mon = mon - 1;
+            }
+            else {
+                year = year - 1;
+                mon = 12;
+            }
+        }
+        d.setDate(d.getDate() - n);
+        year = d.getFullYear()+"";
+        mon = d.getMonth() + 1;
+        //   day=d.getDate();      s = year+"-"+(mon<10?('0'+mon):mon)+"-"+(day<10?('0'+day):day);//日期类型2019-03-07
+        day = d.getDate();
+        s = year + (mon < 10 ? ('0' + mon) : mon) + (day < 10 ? ('0' + day) : day);//日期类型20190307(字符串)
+
+        return s;
+
+    }
+    function MyDate1(n) {
         var n = n;
         var d = new Date();
         var year = d.getFullYear();
